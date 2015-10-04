1bec356cc444899a5dae20870dfa10b48f10813e
DevChagrins <malcolmjgruber@gmail.com>
2015-10-04 12:54:16 -0400

    Initial Project Upload
    Both tool and game base.

---
+++ TreeBuilder/Assets/Scripts/BaseNode.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class BaseNode : MonoBehaviour
+{
+    public SpriteRenderer NodeFrame = null;
+
+    public bool ChangeColor = false;
+    public int ColorMode = 0;
+
+    void Awake()
+    {
+        ChangeColor = true;
+    }
+
+	// Use this for initialization
+	void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if(ChangeColor == true)
+        {
+            switch(ColorMode)
+            {
+                case 0:
+                    {
+                        ChangeOutlineColor(Color.white);
+                        break;
+                    }
+                case 1:
+                    {
+                        ChangeOutlineColor(Color.blue);
+                        break;
+                    }
+                case 2:
+                    {
+                        ChangeOutlineColor(Color.green);
+                        break;
+                    }
+            }
+            ChangeColor = false;
+        }
+	}
+
+    private bool ChangeOutlineColor(Color newColor)
+    {
+        bool didChange = false;
+
+        if (NodeFrame != null)
+        {
+            NodeFrame.color = newColor;
+        }
+
+        return didChange;
+    }
+}
